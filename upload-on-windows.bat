a16e55062c4dc56ee504671f04bc826b0e436a1a
Trytotryto <2310064298@qq.com>
2019-02-26 16:06:42 +0800

    modify post

--- upload-on-windows.bat
+++ upload-on-windows.bat
@@ -1,5 +1,6 @@
 git config --local user.name "Trytotryto"
 git add -A
 git commit -m "modify post"
+git push git@github.com:trytotryto/trytotryto.github.io.git raw
 hexo clean | hexo g -d
 pause
--- upload.sh
+++ upload.sh
@@ -3,5 +3,5 @@ git config --local user.name "Trytotryto"
 #git config --local user.email ""
 git add -A
 git commit -m "modify post"
-#git push git@github.com:Trytotryto/trytotryto.github.io.git raw
+git push git@github.com:Trytotryto/trytotryto.github.io.git raw
 hexo clean | hexo g -d
